1d28ed22353c6f38356ccce106e2eb265dccd20d
LiuToki <ray.usaneko@gmail.com>
2021-08-03 21:52:32 +0900

    [gas-typescript] Create template.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// For typescript Library.
+
+import { sampleModule } from "./sample";
+
+export namespace GASTypescript {
+	export function DoWork(): string {
+		return sampleModule();
+	};
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// For GAS Library.
+import { sampleModule } from "./sample";
+
+export function DoWork(): string {
+	return sampleModule();
+}
